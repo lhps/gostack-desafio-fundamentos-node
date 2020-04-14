bcee233ecfcf5f4244308011359f773b95d895e6
Lucas Pinho <lucashpsfei@gmail.com>
2020-04-14 13:40:41 -0300

    Desafio finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -15,14 +21,42 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+
+    const income = this.transactions.reduce((result, transaction) => {
+      if (transaction.type === 'income') return result + transaction.value;
+
+      return result;
+    }, 0);
+
+    const outcome = this.transactions.reduce((result, transaction) => {
+      if (transaction.type === 'outcome') return result + transaction.value;
+
+      return result;
+    }, 0);
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
